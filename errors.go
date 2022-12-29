f0042b7e6f9ec4b26c75deb4535b514bdbc1403d
gianluca <gepisolo@gmail.com>
2022-12-29 15:58:02 +0100

    feat(module): add more errors

--- errors.go
+++ errors.go
@@ -1,5 +1,7 @@
 package utils
 
+import "net/http"
+
 const (
 	ErrorFirebaseEmailMissing         = "no firebase email available"
 	ErrorBlockedEmailDomain           = "blocked-email-domain"
@@ -27,3 +29,47 @@ const (
 	ErrorConcurrency                  = "record-was-already-changed"
 	ErrorSubscriptionItemPlanNotFound = "subscription-item-plan-not-found"
 )
+
+type NotFound struct {
+	Err error
+}
+
+func (e NotFound) Error() string {
+	return ErrorResourceNotFound
+}
+
+func (e NotFound) Code() int {
+	return 404
+}
+
+type Forbidden struct {
+	Err error
+}
+
+func (e Forbidden) Error() string {
+	if e.Err == nil {
+		return "no rights to access the content"
+	}
+
+	return e.Err.Error()
+}
+
+func (e Forbidden) Code() int {
+	return 403
+}
+
+type InvalidRequestError struct {
+	Err error
+}
+
+func (e InvalidRequestError) Error() string {
+	if e.Err == nil {
+		return "invalid-request"
+	}
+
+	return e.Err.Error()
+}
+
+func (e InvalidRequestError) Code() int {
+	return http.StatusUnprocessableEntity
+}
