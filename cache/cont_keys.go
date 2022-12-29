1ce042feeea04a47cd9b6c3df704cccc15825fa8
gianluca <gepisolo@gmail.com>
2022-12-29 16:12:45 +0100

    feat(module): add skills redis keys

---
+++ cache/cont_keys.go
@@ -0,0 +1,9 @@
+package cache
+
+const (
+	KeyGetUserSkillsFeedback = "m|sk|GUf"
+	KeyGetSkill              = "m|sk|G1"
+	KeySearchSkills          = "m|sk|S"
+	KeyGetSkills             = "m|sk|Ga"
+	KeyContV2FsSkills        = "m|sk|h"
+)
