4f4411cfe9f612c8fe3e4c5c9d87c6f8bce35511
gianluca <gepisolo@gmail.com>
2023-02-06 12:26:32 +0100

    feat(game): add request

---
+++ game/requests.go
@@ -0,0 +1,15 @@
+package game
+
+import "time"
+
+type SkillXP struct {
+	SkillId uint32 `json:"skill_id" validate:"required" gorm:"primaryKey"`
+	Locale  string `json:"locale" validate:"required" gorm:"primaryKey"`
+	XP      int    `json:"xp"`
+}
+
+type SkillXPRequest struct {
+	UserId   uint32     `json:"user_id" validate:"required"`
+	SkillXps []SkillXP  `json:"skill_xps" validate:"required,dive"`
+	Ts       *time.Time `json:"ts"`
+}
