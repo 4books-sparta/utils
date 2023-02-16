da0630407bc93e0b18ec34ac0823c0e04e806e84
gianluca <gepisolo@gmail.com>
2023-02-16 17:15:50 +0100

    feat(kafka): add tls dialer

--- kafka2/producer.go
+++ kafka2/producer.go
@@ -2,9 +2,11 @@ package kafka2
 
 import (
 	"context"
+	"crypto/tls"
 	"errors"
 	"fmt"
 	"log"
+	"net"
 	"os"
 	"strings"
 	"sync"
@@ -51,6 +53,15 @@ func KafkaProducerCreate(opts ...KafkaOption) (*KafkaProducer, error) {
 
 	if nop, err := KafkaAuth(k.cfg); err == nil && nop != nil {
 		kopts = append(kopts, nop)
+	}
+
+	//Use TLS?
+	if k.cfg.dialTLS != nil {
+		tlsDialer := &tls.Dialer{NetDialer: &net.Dialer{Timeout: 10 * time.Second}}
+		kopts = append(kopts, kgo.Dialer(tlsDialer.DialContext))
+		if k.cfg.verbose {
+			fmt.Println("TLS dialer set")
+		}
 	}
 
 	switch strings.ToLower(k.cfg.compression) {
