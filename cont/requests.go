3871f9c3fd962bef3865df02356124a3f58003b6
gianluca <gepisolo@gmail.com>
2023-03-07 15:14:38 +0100

    feat(requests): add req/res

--- cont/requests.go
+++ cont/requests.go
@@ -4,3 +4,8 @@ type SkillBooksMatchRequest struct {
 	Lang  string   `json:"-" validate:"required"`
 	Books []string `json:"books" validate:"required"`
 }
+
+type SkillsBooksMatchRequest struct {
+	Lang   string   `json:"-" validate:"required"`
+	Skills []uint32 `json:"skills" validate:"required"`
+}
--- cont/responses.go
+++ cont/responses.go
@@ -3,3 +3,7 @@ package cont
 type SkillBooksMatchResponse struct {
 	Matches map[uint32]int `json:"matches"`
 }
+
+type BooksMatchResponse struct {
+	Matches map[int][]string `json:"matches"`
+}
