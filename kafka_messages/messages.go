6b11e9a899175aebcd65855d853f76c6306c1826
gianluca <gepisolo@gmail.com>
2023-03-30 15:19:59 +0200

    feat(kafka): time to pointers in event

--- kafka_messages/messages.go
+++ kafka_messages/messages.go
@@ -72,13 +72,13 @@ const (
 )
 
 type IntercomEvent struct {
-	UserId    uint32    `json:"u"`
-	EventName string    `json:"e"`
-	Locale    string    `json:"l,omitempty"`
-	ItemType  string    `json:"i,omitempty"`
-	ItemId    string    `json:"id,omitempty"`
-	Platform  string    `json:"p,omitempty"`
-	DateStart time.Time `json:"ds,omitempty"`
-	DateEnd   time.Time `json:"de,omitempty"`
-	Ts        time.Time `json:"t"`
+	UserId    uint32     `json:"u"`
+	EventName string     `json:"e"`
+	Locale    string     `json:"l,omitempty"`
+	ItemType  string     `json:"i,omitempty"`
+	ItemId    string     `json:"id,omitempty"`
+	Platform  string     `json:"p,omitempty"`
+	DateStart *time.Time `json:"ds,omitempty"`
+	DateEnd   *time.Time `json:"de,omitempty"`
+	Ts        time.Time  `json:"t"`
 }
