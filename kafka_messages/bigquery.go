4b8c6db76763e30d0481dee4a8c5b87058d46d45
gianluca <gepisolo@gmail.com>
2023-05-05 11:09:57 +0200

    feat(messages): add bigquery types

---
+++ kafka_messages/bigquery.go
@@ -0,0 +1,44 @@
+package kafka_messages
+
+import (
+	"errors"
+	"time"
+
+	"gopkg.in/go-playground/validator.v9"
+)
+
+const (
+	ErrorUnknownType            = "unknown-type"
+	ErrorWrongDataInterfaceType = "wrong-data-signature"
+	BigqueryTypeUserLocation    = "ul"
+)
+
+type BigqueryUserLocation struct {
+	UserId    uint32     `json:"user_id" validate:"required"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+	Country   string     `json:"country,omitempty"`
+	City      string     `json:"city,omitempty"`
+}
+
+type BigqueryMsg struct {
+	Type string
+	Data interface{}
+	Log  func(string)
+}
+
+func (msg *BigqueryMsg) Validate() error {
+	validate := validator.New()
+
+	switch msg.Type {
+	case BigqueryTypeUserLocation:
+		if _, ok := msg.Data.(BigqueryUserLocation); !ok {
+			if msg.Log != nil {
+				msg.Log(ErrorWrongDataInterfaceType)
+			}
+			return errors.New(ErrorWrongDataInterfaceType)
+		}
+		//Validate struct
+		return validate.Struct(msg.Data)
+	}
+	return errors.New(ErrorUnknownType)
+}
