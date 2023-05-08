d68caedd8036b06080c27fff837b3aff38ff569e
gianluca <gepisolo@gmail.com>
2023-05-08 11:09:21 +0200

    feat(googlecloud): add bigquery client

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/4books-sparta/utils
 go 1.19
 
 require (
+	cloud.google.com/go/bigquery v1.42.0
 	github.com/aws/aws-sdk-go v1.40.45
 	github.com/getsentry/raven-go v0.2.0
 	github.com/go-kit/kit v0.12.0
@@ -21,6 +22,10 @@ require (
 )
 
 require (
+	cloud.google.com/go v0.104.0 // indirect
+	cloud.google.com/go/compute v1.12.1 // indirect
+	cloud.google.com/go/compute/metadata v0.2.1 // indirect
+	cloud.google.com/go/iam v0.3.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/certifi/gocertifi v0.0.0-20210507211836-431795d63e8d // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
@@ -31,8 +36,13 @@ require (
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.13.0 // indirect
@@ -64,8 +74,16 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
+	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8 // indirect
+	golang.org/x/net v0.0.0-20221014081412-f15817d10f9b // indirect
+	golang.org/x/oauth2 v0.0.0-20221014153046-6fdb5e3db783 // indirect
 	golang.org/x/sys v0.0.0-20220908164124-27713097b956 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/api v0.102.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20221024183307-1bc688fe9f3e // indirect
+	google.golang.org/grpc v1.50.1 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
---
+++ googlecloud/bigquery.go
@@ -0,0 +1,106 @@
+package googlecloud
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"cloud.google.com/go/bigquery"
+)
+
+const BigQueryErrPrefix = "Google-Bigquery-"
+
+type BigqueryClient struct {
+	client  *bigquery.Client
+	config  *BigQueryConfig
+	Verbose bool
+}
+
+func NewBigqueryClient(cfg *BigQueryConfig) (*BigqueryClient, error) {
+	ret := BigqueryClient{
+		config: cfg,
+	}
+	bqClient, err := bigquery.NewClient(context.Background(), cfg.ProjectId)
+	if err != nil {
+		fmt.Println("Bigquery error", err)
+		return nil, err
+	}
+	ret.client = bqClient
+
+	return &ret, nil
+}
+
+type BigQuerySavePayload struct {
+	Item map[string]bigquery.Value
+}
+
+func (s *BigQuerySavePayload) Save() (map[string]bigquery.Value, string, error) {
+	return s.Item, bigquery.NoDedupeID, nil
+}
+
+type BigQueryConfig struct {
+	Dataset        string
+	ReplicaDataset string
+	ProjectId      string
+}
+
+func GetBigQueryConfig(projectId, dataset, replicaDataset string) *BigQueryConfig {
+	return &BigQueryConfig{
+		ProjectId:      projectId,
+		Dataset:        dataset,
+		ReplicaDataset: replicaDataset,
+	}
+}
+
+func (cl *BigqueryClient) Exec(ctx context.Context, sql string) (*bigquery.RowIterator, error) {
+	if cl.client == nil {
+		return nil, errors.New("bigquery-client-nil")
+	}
+	return cl.client.Query(sql).Read(ctx)
+}
+
+func (cl *BigqueryClient) SaveRecords(table string, payload []*BigQuerySavePayload) error {
+	if cl.client == nil {
+		return errors.New("bigquery-client-nil")
+	}
+
+	dataset := cl.config.Dataset
+
+	ins := cl.client.Dataset(dataset).Table(table).Inserter()
+	err := ins.Put(context.Background(), payload)
+	if err != nil {
+		if multiError, ok := err.(bigquery.PutMultiError); ok {
+			for _, err1 := range multiError {
+				for _, err2 := range err1.Errors {
+					if cl.Verbose {
+						fmt.Println(BigQueryErrPrefix, err2)
+					}
+				}
+			}
+		} else {
+			fmt.Println(BigQueryErrPrefix, err)
+		}
+	}
+	return err
+}
+
+func (cl *BigqueryClient) GetPayload(ev interface{}) (*BigQuerySavePayload, error) {
+	if ev == nil {
+		return nil, nil
+	}
+	payload := BigQuerySavePayload{}
+	jsonString, err := json.Marshal(ev)
+	if err != nil {
+		return nil, err
+	}
+
+	item := make(map[string]bigquery.Value, 0)
+	err = json.Unmarshal(jsonString, &item)
+	if err != nil {
+		return nil, err
+	}
+	payload.Item = item
+
+	return &payload, nil
+}
