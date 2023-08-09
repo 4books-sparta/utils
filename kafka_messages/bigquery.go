530a0bff000dc25be02aa48e59b80ce1027b017c
gianluca <gepisolo@gmail.com>
2023-08-09 11:28:50 +0200

    feat(kafka_messages): bigquery stats added

--- kafka_messages/bigquery.go
+++ kafka_messages/bigquery.go
@@ -14,13 +14,40 @@ const (
 	BigqueryTypeStatPublishedBooks       = "pb"
 	BigqueryTypeStatPublishedArticles    = "pa"
 	BigqueryTypeStatSignups              = "usu"
+	BigqueryTypeStatSubscriptions        = "u_subs"
 	BigqueryTypeStatCreatedSubscriptions = "t_sub"
 	BigqueryTypeStatActiveSubscriptions  = "a_sub"
 	BigqueryTypeStatActiveTrials         = "a_try"
 	BigqueryTypeStatExpiresSub           = "e_sub"
 	BigqueryTypePublishedSkills          = "psk"
 	BigqueryTypePublishedMagazinePosts   = "pmp"
+	BigquerySubscriptionType             = "sub"
+	BigqueryFreeTrialType                = "free_trial"
+	BigqueryNoTrialType                  = "no_trial"
 )
+
+type BigqueryDailySignups struct {
+	Date     string `json:"date,omitempty"`
+	Year     uint16 `json:"year,omitempty"`
+	Month    uint8  `json:"month,omitempty"`
+	Day      uint8  `json:"day,omitempty"`
+	Lang     string `json:"lang"`
+	Num      uint   `json:"num"`
+	Platform string `json:"platform"`
+}
+
+type BigqueryDailySubs struct {
+	Date  string `json:"date,omitempty"`
+	Year  uint16 `json:"year,omitempty"`
+	Month uint8  `json:"month,omitempty"`
+	Day   uint8  `json:"day,omitempty"`
+	// sub | free_trial | no_trial
+	Type      string `json:"type,omitempty"`
+	Num       uint   `json:"num"`
+	Platform  string `json:"platform,omitempty"`
+	Plan      string `json:"plan"`
+	Provision string `json:"provision"`
+}
 
 type BigqueryUserLocation struct {
 	UserId    uint32     `json:"user_id" validate:"required"`
@@ -41,6 +68,24 @@ func (msg *BigqueryMsg) Validate() error {
 	switch msg.Type {
 	case BigqueryTypeUserLocation:
 		if _, ok := msg.Data.(BigqueryUserLocation); !ok {
+			if msg.Log != nil {
+				msg.Log(ErrorWrongDataInterfaceType)
+			}
+			return errors.New(ErrorWrongDataInterfaceType)
+		}
+		//Validate struct
+		return validate.Struct(msg.Data)
+	case BigqueryTypeStatSignups:
+		if _, ok := msg.Data.(BigqueryDailySignups); !ok {
+			if msg.Log != nil {
+				msg.Log(ErrorWrongDataInterfaceType)
+			}
+			return errors.New(ErrorWrongDataInterfaceType)
+		}
+		//Validate struct
+		return validate.Struct(msg.Data)
+	case BigqueryTypeStatSubscriptions:
+		if _, ok := msg.Data.(BigqueryDailySubs); !ok {
 			if msg.Log != nil {
 				msg.Log(ErrorWrongDataInterfaceType)
 			}
