f3740d1902fced37ceeb41c776f9ceecc732b94a
gianluca <gepisolo@gmail.com>
2023-09-20 15:02:54 +0200

    feat(pub_sub): add GetTopic change Publish signature

--- gc-pubsub/common.go
+++ gc-pubsub/common.go
@@ -16,8 +16,10 @@ const (
 )
 
 type Client struct {
-	client *pubsub.Client
-	debug  bool
+	client       *pubsub.Client
+	debug        bool
+	activeTopics map[string]*pubsub.Topic
+	mu           sync.Mutex
 }
 
 type ClientConfig struct {
@@ -33,6 +35,34 @@ func NewClient(ctx context.Context, cfg ClientConfig) (*Client, error) {
 		client: cl,
 		debug:  false,
 	}, nil
+}
+
+func (c *Client) SetActiveTopic(t *pubsub.Topic) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if _, ok := c.activeTopics[t.ID()]; !ok {
+		c.activeTopics[t.ID()] = t
+	}
+}
+
+func (c *Client) StopActiveTopics() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.activeTopics == nil {
+		return
+	}
+
+	for _, topic := range c.activeTopics {
+		topic.Stop()
+	}
+}
+
+func (c *Client) GetActiveTopics() map[string]*pubsub.Topic {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	return c.activeTopics
 }
 
 func (c *Client) Log(level, str string) {
@@ -124,11 +154,11 @@ func (c *Client) ListTopics(ctx context.Context) ([]*pubsub.Topic, error) {
 	return topics, nil
 }
 
-func (c *Client) Publish(ctx context.Context, topic, msg string) error {
-	t := c.client.Topic(topic)
+func (c *Client) Publish(ctx context.Context, t *pubsub.Topic, msg string) error {
 	result := t.Publish(ctx, &pubsub.Message{
 		Data: []byte(msg),
 	})
+	c.SetActiveTopic(t)
 	// Block until the result is returned and a server-generated
 	// ID is returned for the published message.
 	id, err := result.Get(ctx)
@@ -147,4 +177,8 @@ func (c *Client) Subscribe(name string) *pubsub.Subscription {
 
 func (c *Client) PubSubClient() *pubsub.Client {
 	return c.client
+}
+
+func (c *Client) GetTopic(name string) *pubsub.Topic {
+	return c.client.Topic(name)
 }
