5aa3119c842e23b861895c282eb7ece8b13c5cd5
gianluca <gepisolo@gmail.com>
2023-10-25 16:21:53 +0200

    feat(reaction): add UserCachedContents

--- cont/reactions/models.go
+++ cont/reactions/models.go
@@ -1,6 +1,9 @@
 package reactions
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 const (
 	ReactionFollowKey = "follow"
@@ -49,4 +52,48 @@ func (f *SkillFeedback) Essentials() *SkillFeedback {
 
 type SkillFollowedResponse struct {
 	Skills []uint32 `json:"skills"`
+}
+
+type UserCachedContents struct {
+	UserId                uint32    `json:"user_id" gorm:"primary_key"`
+	RecommendedArticles   string    `json:"recommended_articles"`
+	RecommendedArticlesAt time.Time `json:"recommended_articles_at"`
+}
+
+type RecommendedArticle struct {
+	WithUUiDRow
+	Score int `json:"s"`
+}
+
+type RecommendedArticles map[string]*RecommendedArticle
+
+type WithUUiDRow struct {
+	ID string `json:"id" sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
+}
+
+func (ucc *UserCachedContents) SetRecommendedArticles(in RecommendedArticles, syncDate bool) error {
+	txt, err := json.Marshal(in)
+	if err != nil {
+		return err
+	}
+
+	ucc.RecommendedArticles = string(txt)
+	if syncDate {
+		ucc.RecommendedArticlesAt = time.Now()
+	}
+	return nil
+}
+
+func (ucc *UserCachedContents) ParseRecommendedArticles() (RecommendedArticles, error) {
+	res := make(RecommendedArticles)
+	if ucc.RecommendedArticles == "" {
+		//empty
+		return res, nil
+	}
+
+	err := json.Unmarshal([]byte(ucc.RecommendedArticles), &res)
+	if err != nil {
+		return res, err
+	}
+	return res, nil
 }
