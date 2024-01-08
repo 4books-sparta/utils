721243333bb91acb2443b623c80c874fd35084a6
gianluca <gepisolo@gmail.com>
2024-01-08 11:29:35 +0100

    feat(intercom): add event The update email opened

--- kafka_messages/intercom.go
+++ kafka_messages/intercom.go
@@ -17,6 +17,8 @@ const (
 	EventCouponAssigned          = "uca"
 	EventCouponConsumed          = "ucc"
 	EventLeadTagged              = "lt"
+	EventTheUpdateEmailOpened    = "teo"
+	KeyNum                       = "num"
 )
 
 type IntercomEvent struct {
@@ -30,4 +32,22 @@ type IntercomEvent struct {
 	DateEnd   *time.Time             `json:"de,omitempty"`
 	Ts        time.Time              `json:"t"`
 	Data      map[string]interface{} `json:"data,omitempty"`
+}
+
+func (ev *IntercomEvent) SetTheUpdateEmailOpenedNum(num uint) {
+	data := make(map[string]interface{})
+	data[KeyNum] = num
+	ev.Data = data
+}
+
+func (ev *IntercomEvent) GetTheUpdateEmailOpenedNum() uint {
+	if ev.Data == nil {
+		return 0
+	}
+	if num, ok := ev.Data[KeyNum]; ok {
+		if intV, ok2 := num.(uint); ok2 {
+			return intV
+		}
+	}
+	return 0
 }
